a8c41f6c1b5e8ca2ece95d10bd5568923aaa16c4
AOREC <98394423+Aorec-XD@users.noreply.github.com>
2022-10-19 18:11:37 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+os.system("git pull")
+try:
+     os.mkdir('result')
+except:pass
+if __name__ == "__main__":
+        try:
+                __import__("gxrex").lisensi()
+        except Exception as e:
+                exit(str(e))
