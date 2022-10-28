b71c117d9cdb3dabf51fbb9f78916b0adb7e2fa4
AOREC <98394423+Aorec-XD@users.noreply.github.com>
2022-10-28 09:59:29 +0700

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import os
-try:
- os.mkdir('result')
-except:pass
-if __name__ == "__main__":
-        try:
-                __import__("gxrex").lisensi()
-        except Exception as e:
-                exit(str(e))
