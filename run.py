0da6d895628ddcc4bc87688009fae2c518ca032a
AOREC <98394423+Aorec-XD@users.noreply.github.com>
2022-10-28 09:59:56 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+import os
+try:
+ os.mkdir('result')
+except:pass
+if __name__ == "__main__":
+        try:
+                __import__("gxrex").lisensi()
+        except Exception as e:
+                exit(str(e))
